6556b4310750ff307ddd90f6278189df1a28be79
Jesper Danielsson <jesper.danielsson@student.forsbergsskola.se>
2021-10-27 20:41:47 +0200

    Added pumpkinShooter

---
+++ Assets/Scripts/Shooting.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooting : MonoBehaviour
+{
+    public GameObject projectile;
+    public float launchVelocity = 700f;
+    public string fire = "fire1P1";
+
+    private void FixedUpdate()
+    {
+        if (Input.GetButton(fire))
+        {
+            GameObject ball = Instantiate(projectile, transform.position, transform.rotation);
+            ball.GetComponent<Rigidbody2D>().AddRelativeForce(new Vector2(launchVelocity,0));
+            Debug.Log("im shooting");
+        }
+    }
+}
