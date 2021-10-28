4d84a78c56ad3e33cf4ddd6333608461397668e0
Jesper Danielsson <jesper.danielsson@student.forsbergsskola.se>
2021-10-28 15:07:24 +0200

    Added initial script for HealthUI

--- Assets/Scripts/HealthUIConnector.cs
+++ Assets/Scripts/HealthUIConnector.cs
@@ -2,11 +2,44 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class HealthUIConnector : MonoBehaviour
 {
+
+    public Canvas hpCanvas;
+    
+    private int player1Health;
+    private int player2Health;
+    private void Start()
+    {
+        PlayerHealth[] players = FindObjectsOfType<PlayerHealth>();
+        foreach (var VARIABLE in players)
+        {
+            if (VARIABLE.gameObject.name == "p_Player_P1")
+            {
+                player1Health = VARIABLE._health;
+            }
+
+            if (VARIABLE.gameObject.name == "p_Player_P2")
+            {
+                player2Health = VARIABLE._health;
+            }
+        }
+    }
+
     private void Update()
     {
+        if (hpCanvas.GetComponentInChildren<Text>().name == "Player1")
+        {
+            string text = Convert.ToString(player1Health);
+        }
+
+        if (hpCanvas.GetComponentInChildren<Text>().name == "Player2")
+        {
+            string text = Convert.ToString(player2Health);
+        }
+            
         
     }
 }
