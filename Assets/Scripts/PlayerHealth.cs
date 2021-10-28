6006e9a207e29443aa64698fe50c2c92f8772e9d
Jesper Danielsson <jesper.danielsson@student.forsbergsskola.se>
2021-10-28 11:15:31 +0200

    tweaking of multiplayer shooting

--- Assets/Scripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerHealth.cs
@@ -2,29 +2,40 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerHealth : MonoBehaviour
 {
     public int maxHealth = 250;
 
     private int _health;
+    private Scene _current;
 
     private void Start()
     {
         _health = maxHealth;
+        _current = SceneManager.GetActiveScene();
     }
 
     private void OnCollisionEnter2D(Collision2D other)
     {
-        
-            
-            if (other.gameObject.CompareTag("Projectile"))
+        if (other.gameObject.CompareTag("Projectile"))
+        {
+            if (other.relativeVelocity.magnitude > 20)
             {
-                if (other.relativeVelocity.magnitude > 2)
-                {
-                    _health -= 20;
-                }
+                _health -= 5;
+                Debug.Log(_health);
             }
-        
+        }
+        Death();
+    }
+
+    void Death()
+    {
+        if (_health <= 0)
+        {
+            Destroy(this);
+            SceneManager.LoadScene(_current.buildIndex);
+        }
     }
 }
