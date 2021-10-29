3d2151c8ec26042dae73c4fa36ef82aad49891c8
Jesper Danielsson <jesper.danielsson@student.forsbergsskola.se>
2021-10-29 12:44:17 +0200

    Damage sounds implemented

--- Assets/Scripts/Audio/SoundControlP2.cs
+++ Assets/Scripts/Audio/SoundControlP2.cs
@@ -7,6 +7,7 @@ public class SoundControlP2 : MonoBehaviour
     public static SoundControlP2 SndMan;
     private AudioSource audioSrc;
     private AudioClip[] jumpSounds;
+    private AudioClip[] damageSounds;
     private int random;
     public string jumper = "JumpSoundsP1";
 
@@ -15,11 +16,17 @@ public class SoundControlP2 : MonoBehaviour
         SndMan = this;
         audioSrc = GetComponent<AudioSource>();
         jumpSounds = Resources.LoadAll<AudioClip>(jumper);
+        damageSounds = Resources.LoadAll<AudioClip>("DamageSoundsP2");
     }
 
     public void PlayJumpSoundsP2()
     {
         random = Random.Range(0, 3);
         audioSrc.PlayOneShot(jumpSounds[random]);
+    }
+    public void PlayDamageSoundsP2()
+    {
+        random = Random.Range(0, 3);
+        audioSrc.PlayOneShot(damageSounds[random]);
     }
 }
